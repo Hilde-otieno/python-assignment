2d5be889a0b469b0cb4a94d59002acaa9b46c510
Esther-Naserian <yemanehaxumawit@gmail.com>
2024-07-15 22:21:19 +0300

    made changes

--- api/views.py
+++ api/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
-from api.serializer import Class_PeriodSerializer, CourseSerializer, Student_ClassSerializer, StudentSerializer, TeacherSerializer
+
+from api.serializer import Class_PeriodSerializer, CourseSerializer, ClassroomSerializer, StudentSerializer, TeacherSerializer
 from classperiod.models import Class_Period
-from course.models import Course
+from course.models import Courses
 from student.models import Student
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from classroom.models import Student_Class
+from classroom.models import Classroom
 from teacher.models import Teacher
 # Create your views here.
 
@@ -14,20 +15,20 @@ class StudentListViews(APIView):
         student = Student.objects.all()
         serializer = StudentSerializer(student, many=True)
         return Response(serializer.data)
-class Teacher                                                                                                           ListViews(APIView):
+class TeacherListViews(APIView):                                                                                                  
     def get(self, request):
         teacher = Teacher.objects.all()
         serializer = TeacherSerializer(teacher, many=True)
         return Response(serializer.data)
 class CourseListViews(APIView):
     def get(self, request):
-        course = Course.objects.all()
+        course = Courses.objects.all()
         serializer = CourseSerializer(course, many=True)
         return Response(serializer.data)
 class ClassListViews(APIView):
     def get(self, request):
-        student_class = Student_Class.objects.all()
-        serializer = Student_ClassSerializer(student_class, many=True)
+        Classroom = Classroom.objects.all()
+        serializer = ClassroomSerializer(Classroom, many=True)
         return Response(serializer.data)
 class Class_PeriodListViews(APIView):
     def get(self, request):
